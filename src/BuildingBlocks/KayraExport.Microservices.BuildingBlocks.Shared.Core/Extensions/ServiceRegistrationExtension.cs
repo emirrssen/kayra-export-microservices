64a5bd6b2c816274bd67a89f45804e4dc77221d5
emirrssen <emirrssen@gmail.com>
2026-03-25 22:00:09 +0300

    feat(shared): implement centralized service registration for mediatr and fluentvalidation

---
+++ src/BuildingBlocks/KayraExport.Microservices.BuildingBlocks.Shared.Core/Extensions/ServiceRegistrationExtension.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace KayraExport.Microservices.BuildingBlocks.Shared.Core.Extensions
+{
+    public static class ServiceRegistrationExtension
+    {
+        /// <summary>
+        /// Ortak kütüphaneleri çağırıldıkları projelerde ilgili assembly'ye göre register eder.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="assembly"></param>
+        public static void AddSharedCoreServices(this IServiceCollection services, Assembly assembly)
+        {
+            // MediatR'ın çağırıldığı projede eklenmesini sağlar.
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
+
+            // Fluent validation'ın çağırıldığı projede eklenmesini sağlar.
+            services.AddValidatorsFromAssembly(assembly);
+        }
+    }
+}
