f693e6d79ab57a66a2b06365ba0d858e625760d3
emirrssen <emirrssen@gmail.com>
2026-03-29 22:16:22 +0300

    feat(gateway): integrate reverse proxy routing and rate limiting
    - Integrate reverse proxy infrastructure to centralize and manage microservice routing.
    
    - Configure upstream routing definitions and downstream clusters for Auth and Product services.
    
    - Implement fixed-window rate limiting policies to prevent service abuse and throttle traffic.
    
    - Register reverse proxy and rate limiting middleware within the API Gateway request pipeline.

--- src/Gateway/KayraExport.Microservices.Gateway.API_Gateway/Program.cs
+++ src/Gateway/KayraExport.Microservices.Gateway.API_Gateway/Program.cs
@@ -1,4 +1,11 @@
+using KayraExport.Microservices.Gateway.API_Gateway;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddServices(builder.Configuration);
+
 var app = builder.Build();
 
+app.UseRateLimiter();
+app.MapReverseProxy();
 app.Run();
---
+++ src/Gateway/KayraExport.Microservices.Gateway.API_Gateway/ServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
+namespace KayraExport.Microservices.Gateway.API_Gateway
+{
+    public static class ServiceRegistration
+    {
+        public static void AddServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddRateLimiter(options =>
+            {
+                options.AddFixedWindowLimiter("fixed-5-per-second", opt =>
+                {
+                    opt.PermitLimit = 5;
+                    opt.Window = TimeSpan.FromSeconds(1);
+                    opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                    opt.QueueLimit = 0;
+                });
+
+                options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+            });
+
+            services.AddReverseProxy()
+                .LoadFromConfig(config.GetSection("ReverseProxy"));
+        }
+    }
+}
